49e24a737ad5b274fb90bd854643d64507d435c3
Nick Gawenda <ngawenda@emich.edu>
2025-10-21 17:57:41 -0400

    working

--- COSC541 Foundations of Automata and Languages/All Course Files/PYTHON/Exercise 1.py
+++ COSC541 Foundations of Automata and Languages/All Course Files/PYTHON/Exercise 1.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python3
-import re;
+import re
 
-s = "aabb";
+s = "aabb"
 
 
 # (a^n b^m; n==m)
-s1 = "aaabbb"
-n = 0
-for m in re.finditer(r"b", s1):
-    n+=1;
-print(n)
-print(bool(re.fullmatch(r"a{n}b{n}", s1)));
+s1 = "ab"
+n = re.findall(r"a", s1).__len__()
+m = re.findall(r"b", s1).__len__()
+print((bool(n == m) & bool(re.findall(r"ba", s1).__len__() == 0 )))
 
-s2 = "abc </p> <p> first </p> <p> second </p> n </p> <p> a";
-print(re.findall(r"[<]p>[^/]*/p[>]", s2))
+s2 = "abc </p> <p> first </p> <p> second </p> n </p> <p> a"
+print(re.findall(r"<p>[^/]*/p>", s2))
 
 s3 = "123"
-print(bool(re.fullmatch(r"\w{3,12}" , s3)));
+print(bool(re.fullmatch(r"\w{3,12}" , s3)))
 
 s4 = "1234-5678-9012-3456"
 print(re.sub(r"\d{4}-\d{4}-\d{4}", "XXXX-XXXX-XXXX",s4))
 
 s5 = "aabbb"
-print(bool(re.fullmatch(r"aaa*bbbb*", s5)))
+print(bool(re.fullmatch(r"aa+bbb+", s5)))
